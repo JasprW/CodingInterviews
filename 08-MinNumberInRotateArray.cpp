05c1f6e39ba4f584f9f4542e36ab95a005ec9e63
Jaspr Wang <xiaop987@gmail.com>
2019-02-19 10:30:18 +0800

    complete 08

---
+++ 08-MinNumberInRotateArray.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minNumberInRotateArray(vector<int> rotateArray) {
+        int size = rotateArray.size();
+        if (size == 0) {
+            return 0;
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                if (rotateArray[i] > rotateArray[i + 1]) {
+                    return rotateArray[i + 1];
+                }
+            }
+            return rotateArray[0];
+        }
+    }
+};
