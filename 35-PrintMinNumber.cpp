a602b4ecda0e2503759a963c4ca904da94f8fe0e
Jaspr Wang <xiaop987@gmail.com>
2019-03-12 21:36:02 +0800

    Complete 35-37

---
+++ 35-PrintMinNumber.cpp
@@ -0,0 +1,30 @@
+#include <vector>;
+#include <string>;
+#include <sstream>;
+using namespace std;
+
+class Solution {
+public:
+    string PrintMinNumber(vector<int> numbers) {
+        string result;
+        if (numbers.size() <= 0) return result;
+        vector<string> strNum;
+        for(int i=0; i<numbers.size(); i++) {
+            stringstream ss;
+            ss << numbers[i];
+            string s = ss.str();
+            strNum.push_back(s);
+        }
+        sort(strNum.begin(), strNum.end(), compare);
+        for (int i=0; i<strNum.size(); i++) {
+            result.append(strNum[i]);
+        }
+        return result;
+    }
+
+    static bool compare(const string &str1, const string &str2) {
+        string s1 = str1 + str2;
+        string s2 = str2 + str1;
+        return s1 < s2;
+    }
+};
---
+++ 36-GetUglyNumber.cpp
@@ -0,0 +1,21 @@
+#include <queue>;
+using namespace std;
+
+class Solution {
+public:
+    int GetUglyNumber_Solution(int index) {
+        if(index < 7) 
+            return index;
+        int p2 = 0, p3 = 0, p5 = 0, newNum = 1;
+        vector<int> arr;
+        arr.push_back(newNum);
+        while(arr.size() < index) {
+            newNum = min(arr[p2] * 2, min(arr[p3] * 3, arr[p5] * 5));
+            if(newNum == arr[p2] * 2) p2++;
+            if(newNum == arr[p3] * 3) p3++;
+            if(newNum == arr[p5] * 5) p5++;
+            arr.push_back(newNum);
+        }
+        return newNum;
+    }
+};
